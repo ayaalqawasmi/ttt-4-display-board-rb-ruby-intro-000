82c172589b0a2eff06dee1ec72d827bf1a71c3dc
Aya Alqawasmi <aya.alqawasmi@gmail.com>
2018-01-21 16:54:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,12 +4,8 @@
 
 def display_board(board)
  puts "
-#{board[0]} | #{board[1]} | #{board[3]}
- -----------
-#{board[4]} | #{board[5]} | #{board[6]}
- -----------
-#{board[7]} | #{board[8]} | #{board[9]} "
-end
-board = ["X", "X", "X", " ", "X", " ", "X", "O", " "]
+#{board[0]} | #{board[1]} | #{board[3]} "
+end 
+board = ["X", "X", "X"]
 
 display_board(board)
