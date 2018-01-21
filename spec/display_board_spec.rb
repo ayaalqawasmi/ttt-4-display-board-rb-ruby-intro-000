a7923d2bc0719c13ef756c4748413e7d68ad4f94
Aya Alqawasmi <aya.alqawasmi@gmail.com>
2018-01-21 17:11:53 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -188,3 +188,13 @@ describe "#display_board in 'lib/display_board.rb" do
     end
   end
 end
+it 'prints a 3x3 tic tac toe board' do
+  output = capture_puts{ display_board }
+  output_array = output.split "\n"
+
+   expect(output_array[0]).to include("   |   |   ")
+   expect(output_array[1]).to include("-----------")
+   expect(output_array[2]).to include("   |   |   ")
+   expect(output_array[3]).to include("-----------")
+   expect(output_array[4]).to include("   |   |   ")
+end
